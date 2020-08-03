16e6095984a986397eab69d04358907bffdf7609
S Roy <roy.shantinath@gmail.com>
2020-08-04 00:44:19 +0530

    Formatted few settings added

--- src/services/dataTypes/authData.ts
+++ src/services/dataTypes/authData.ts
@@ -1,26 +1,26 @@
 export default interface AuthorizationData {
-    token: string;
-    route: string;
-    method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
-    isNipigeAdmin: boolean;
-    isNipigeTenant: boolean;
+  token: string;
+  route: string;
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+  isNipigeAdmin: boolean;
+  isNipigeTenant: boolean;
 }
 
 interface AuthorizationDataResponse {
-  _id: string,
-  category: string,
-  tenant: string,
-  roles: [string],
+  _id: string;
+  category: string;
+  tenant: string;
+  roles: [string];
   authentication: {
-    otpVerified: boolean,
-    email: string,
-    userName: string,
-    passwordHash: string
-  },
-  iat: number
+    otpVerified: boolean;
+    email: string;
+    userName: string;
+    passwordHash: string;
+  };
+  iat: number;
 }
 
 export interface AuthorizationResponse {
-  status: number,
-  data: AuthorizationDataResponse
+  status: number;
+  data: AuthorizationDataResponse;
 }
